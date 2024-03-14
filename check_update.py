8cf420b1a52d88e7e0fea9ebad785b63471bd6e2
feiyu201 <77211066+feiyu201@users.noreply.github.com>
2024-03-14 11:56:23 +0800

    Update check_update.py

--- check_update.py
+++ check_update.py
@@ -2,17 +2,18 @@ import requests
 import os
 from git import Repo
 from datetime import datetime
+import pytz
 
 # GitHub API 相关信息
 GITHUB_API_URL = "https://api.github.com/repos/aazooo/zjmf"
-REPO_PATH = "zjmf"  # 本地库路径
+REPO_PATH = "zjmftb"  # 本地库路径
 
 def check_update():
     # 发送GET请求获取最后更新时间
     response = requests.get(GITHUB_API_URL)
     if response.status_code == 200:
         repo_info = response.json()
-        last_updated_at = datetime.strptime(repo_info['updated_at'], '%Y-%m-%dT%H:%M:%SZ')
+        last_updated_at = datetime.strptime(repo_info['updated_at'], '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=pytz.utc)
         
         # 检查本地库是否存在，如果不存在则克隆
         if not os.path.exists(REPO_PATH):
@@ -21,8 +22,8 @@ def check_update():
         # 打开本地库
         repo = Repo(REPO_PATH)
         
-        # 获取本地库的最后提交时间
-        last_commit = repo.head.commit.committed_datetime
+        # 获取本地库的最后提交时间并转换为UTC时间
+        last_commit = repo.head.commit.committed_datetime.replace(tzinfo=pytz.utc)
         
         # 检查是否有更新
         if last_commit < last_updated_at:
